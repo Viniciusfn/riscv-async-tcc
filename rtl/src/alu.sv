772c58f23f7ae87bf74bb707ed612e5205b15850
Vinicius Franca <vini.francan@gmail.com>
2025-04-03 22:16:09 -0300

    [ARISCV_EXEC][ALU] RTL v1.0 compiled

---
+++ rtl/src/alu.sv
@@ -0,0 +1,26 @@
+module alu #(
+   /* PARAMETERS */
+   NBW_DATA       = 32
+)(
+   /* INTERFACE */
+   input  logic [NBW_DATA-1:0]   i_srcA,
+   input  logic [NBW_DATA-1:0]   i_srcB,
+   input  logic [2:0]            i_aluControl,
+   output logic [NBW_DATA-1:0]   o_result,
+   output logic                  o_zero
+);
+
+   always_comb begin
+      case (i_aluControl)
+         3'b000: o_result = i_srcA + i_srcB; // ADD
+         3'b001: o_result = i_srcA - i_srcB; // SUB
+         3'b010: o_result = i_srcA & i_srcB; // AND
+         3'b011: o_result = i_srcA | i_srcB; // OR
+         3'b101: o_result = (($signed(i_srcA)) < ($signed(i_srcB))) ? 32'b1 : 32'b0; // SLT
+         default: o_result = '0;
+      endcase
+   end
+
+   assign o_zero = (o_result == '0) ? 1'b1 : 1'b0;
+
+endmodule
---
+++ rtl/src/ariscv_exec.sv
@@ -0,0 +1,82 @@
+module ariscv_exec #(
+   /* PARAMETERS */
+   parameter NBW_PC                 = 32,
+   parameter NBW_REGISTER           = 32,
+   parameter NBW_ADDR               = 5
+)(
+   /* INTERFACE */
+   input  logic                     em_aclk,
+   input  logic                     rst_async_n,
+   // FROM DECODE
+   input  logic [NBW_REGISTER-1:0]  i_rd1,
+   input  logic [NBW_REGISTER-1:0]  i_rd2,
+   input  logic [NBW_REGISTER-1:0]  i_immExt,
+   input  logic [NBW_ADDR-1:0]      i_wr_addr_reg,
+   input  logic [NBW_PC-1:0]        i_pc_de,
+   input  logic [NBW_PC-1:0]        i_pc_plus4_de,
+   input  logic                     i_regWrite,
+   input  logic [1:0]               i_resultSrc,
+   input  logic                     i_memWrite,
+   input  logic                     i_jump,
+   input  logic                     i_branch,
+   input  logic [2:0]               i_aluControl,
+   input  logic                     i_aluSrc,
+   // TO MEMORY
+   output logic [NBW_REGISTER-1:0]  o_aluResult,
+   output logic [NBW_REGISTER-1:0]  o_writeData,
+   output logic [NBW_ADDR-1:0]      o_wr_addr_reg,
+   output logic [NBW_PC-1:0]        o_pc_plus4_em,
+   // TO PC
+   output logic [NBW_PC-1:0]        o_pcTarget,
+   output logic                     o_PCSrc
+);
+   /* Local signals and parameters */
+   logic [NBW_REGISTER-1:0]         alu_srcB_w;
+   logic                            zero_w;
+   logic  [NBW_REGISTER-1:0]        aluResult_w, aluResult_ff;
+   logic  [NBW_REGISTER-1:0]        writeData_w, writeData_ff;
+   logic  [NBW_ADDR-1:0]            wr_addr_reg_w, wr_addr_reg_ff;
+   logic  [NBW_PC-1:0]              pc_plus4_em_w, pc_plus4_em_ff;
+
+   /* Output Assignments */
+   assign o_aluResult = aluResult_ff;
+   assign o_writeData = writeData_ff;
+   assign o_wr_addr_reg = wr_addr_reg_ff;
+   assign o_pc_plus4_em = pc_plus4_em_ff;
+
+   /* Assignments */
+   assign alu_srcB_w = (i_aluSrc) ? i_immExt : i_rd2;
+   assign o_pcTarget = i_immExt + i_pc_de;
+   assign o_PCSrc = i_jump || (i_branch && zero_w);
+   assign writeData_w = i_rd2;
+   assign wr_addr_reg_w = i_wr_addr_reg;
+   assign pc_plus4_em_w = i_pc_plus4_de;
+
+   /* FF */
+   always_ff @(posedge em_aclk or negedge rst_async_n) begin : em_reg
+      if(!rst_async_n) begin
+         aluResult_ff <= '0;
+         writeData_ff <= '0;
+         wr_addr_reg_ff <= '0;
+         pc_plus4_em_ff <= '0;
+      end
+      else begin
+         aluResult_ff <= aluResult_w;
+         writeData_ff <= writeData_w;
+         wr_addr_reg_ff <= wr_addr_reg_w;
+         pc_plus4_em_ff <= pc_plus4_em_w;
+      end
+   end
+
+   /* Instances */
+   alu #(
+      .NBW_DATA      (NBW_REGISTER)
+   ) uu_alu (
+      .i_srcA        (i_rd1),
+      .i_srcB        (alu_srcB_w),
+      .i_aluControl  (i_aluControl),
+      .o_result      (aluResult_w),
+      .o_zero        (zero_w)
+   );
+
+endmodule
