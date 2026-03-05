7b23cb0127ba4ab3126efcf532d6f942291f294f
Vinicius Franca <vini.francan@gmail.com>
2026-03-04 22:30:12 -0300

    [ASYNC][SYNTH] fast corner synth (pending mem delay update)

--- scripts/genus/io_delays.tcl
+++ scripts/genus/io_delays.tcl
@@ -7,12 +7,13 @@
 
 set PC_IO_DELAY      [expr 0.2*${PERIOD_PC_0}]
 set FD_IO_DELAY      [expr 0.2*${PERIOD_FD_1}]
-set MEM_IO_DELAY     [expr 0.2*${PERIOD_MW_4}]
+set EM_IO_DELAY      [expr 0.2*${PERIOD_EM_3}]
+set MEM_IO_DELAY     0.100 ;# Update with reference value
 set SYNC_IO_DELAY    [expr 0.2*${PERIOD_SYNC_CLK}]
 
 if { ${SYNC_VERSION} == 0 } {
-    set_input_delay  -max ${FD_IO_DELAY} -clock [get_clocks {ACLK_PC_0}] [get_ports {i_inst}]
-    set_input_delay  -min 0.001          -clock [get_clocks {ACLK_PC_0}] [get_ports {i_inst}]
+    set_input_delay  -max ${MEM_IO_DELAY} -clock [get_clocks {ACLK_PC_0}] [get_ports {i_inst}]
+    set_input_delay  -min 0.001           -clock [get_clocks {ACLK_PC_0}] [get_ports {i_inst}]
 
     set_output_delay -max ${PC_IO_DELAY} -clock [get_clocks {ACLK_PC_0}] [get_ports {o_pc}]
     set_output_delay -min 0.001          -clock [get_clocks {ACLK_PC_0}] [get_ports {o_pc}]
